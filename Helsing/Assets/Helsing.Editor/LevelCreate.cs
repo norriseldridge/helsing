6a6c4595b13be578644f1de2e21736083bbbc8c8
Norris Eldridge <norris.eldridge@gmail.com>
2021-06-06 17:17:39 -0700

    Created a level template
    Create a starter level template we can use when trying to create new levels
    Also created a new "Create Level" menu item under Helsing/Scenes/Create Level" to make the process of creating new level scenes very fast and easy

---
+++ Helsing/Assets/Helsing.Editor/LevelCreate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Helsing.Editor
+{
+    public static class LevelCreate
+    {
+        const string TEMPLATE = "Assets/Scenes/LevelTemplate.unity";
+
+        [MenuItem("Helsing/Scene/Create Level")]
+        public static void CreateLevel()
+        {
+            string file = EditorUtility.SaveFilePanelInProject("Create Level",
+                "NewLevel",
+                "unity",
+                "Create Level",
+                "Assets/Scenes");
+
+            if (file == null)
+                return;
+
+            FileUtil.CopyFileOrDirectory(TEMPLATE, file);
+            AssetDatabase.Refresh();
+            EditorSceneManager.OpenScene(file);
+        }
+    }
+}
