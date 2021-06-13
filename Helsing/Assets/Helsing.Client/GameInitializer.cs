9ae67335ae87af42f11308125c30fc95df76f902
Norris Eldridge <norris.eldridge@gmail.com>
2021-06-12 23:37:00 -0700

    Clean up more stuff!
    Game Initializer properly works to load the current level you have open in editor, much faster for playing and testing levels
    Fixed the player transform reference in the camera in the level template
    Deleting testing level

--- Helsing/Assets/Helsing.Client/GameInitializer.cs
+++ Helsing/Assets/Helsing.Client/GameInitializer.cs
@@ -1,3 +1,26 @@
-﻿using Helsing.Client.World;using UnityEngine;using UnityEditor;namespace Helsing.Client{    public class GameInitializer : MonoBehaviour    {        private void Start()        {
+﻿using Helsing.Client.World;using UnityEngine;namespace Helsing.Client{    public class GameInitializer : MonoBehaviour    {        private void Start()        {
             // TODO load game state?
-            var currentScene = EditorApplication.currentScene;            // TODO load main menu?            // TODO don't do this            LevelLoader.Load("SampleScene");        }    }}
+
+            // TODO load main menu?
+
+            // TODO don't do this
+#if UNITY_EDITOR            var currentScene = PlayerPrefs.GetString("currentScene", "SampleScene");            LevelLoader.Load(currentScene);
+#else
+            LevelLoader.Load("SampleScene");
+#endif        }
+
+#if UNITY_EDITOR        [UnityEditor.InitializeOnLoadMethod]        private static void OnEditMode()
+        {
+            UnityEditor.EditorApplication.playModeStateChanged -= OnExitingEditMode;
+            UnityEditor.EditorApplication.playModeStateChanged += OnExitingEditMode;
+        }        private static void OnExitingEditMode(UnityEditor.PlayModeStateChange stateChange)
+        {
+            switch (stateChange)
+            {
+                case UnityEditor.PlayModeStateChange.ExitingEditMode:
+                    var currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+                    PlayerPrefs.SetString("currentScene", currentScene);
+                    break;
+            }
+        }
+#endif    }}
