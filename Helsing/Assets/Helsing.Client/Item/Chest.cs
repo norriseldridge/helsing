e9e6b260b72cfc976bc390838082f166fb564bb6
Norris Eldridge <norris.eldridge@gmail.com>
2021-06-19 23:54:37 -0700

    Added Chests into the game
    You can now add chests which are basically mystery items
    You can add any item as being inside the chest but the benefit is the player can't tell what it is
    This could be useful for creating tension on if getting the chest is worth it, it might be extra health or a key but it might be rubbish too

---
+++ Helsing/Assets/Helsing.Client/Item/Chest.cs
@@ -0,0 +1,56 @@
+﻿using Helsing.Client.Audio.Api;
+using Helsing.Client.Item.Api;
+using UnityEngine;
+using Zenject;
+
+namespace Helsing.Client.Item
+{
+    [ExecuteAlways]
+    [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(Collider2D))]
+    public class Chest : MonoBehaviour, IItem
+    {
+        [SerializeField] BaseItem item;
+        [SerializeField] Sprite opened;
+        [SerializeField] AudioClip openSfx;
+
+        SpriteRenderer spriteRenderer;
+        new Collider2D collider;
+        bool collected = false;
+        IAudioPool audioPool;
+
+        public IItemData ItemData => item.ItemData;
+
+        [Inject]
+        private void Inject(IAudioPool audioPool) =>
+            this.audioPool = audioPool;
+
+        public void OnCollect()
+        {
+            if (collected) return;
+
+            audioPool.Next().PlayOneShot(openSfx);
+            
+            spriteRenderer.sprite = opened;
+            enabled = false;
+            item.OnCollect();
+            collider.enabled = false;
+            collected = true;
+        }
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            collider = GetComponent<Collider2D>();
+        }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            item.transform.position = transform.position;
+            item.GetComponent<SpriteRenderer>().enabled = false;
+            item.GetComponent<Collider2D>().enabled = false;
+        }
+#endif
+    }
+}
