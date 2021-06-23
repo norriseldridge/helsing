8bd3ec5440f78fad028b96db09cdd13841d8daf4
Norris Eldridge <norris.eldridge@gmail.com>
2021-06-22 22:55:33 -0700

    Hunch back monster now drops cross
    This can lead to interesting decisions, is it worth sacrificing a stake to get a cross?

--- Helsing/Assets/Helsing.Client/Entity/Enemy/HunchBackController.cs
+++ Helsing/Assets/Helsing.Client/Entity/Enemy/HunchBackController.cs
@@ -3,6 +3,9 @@ using Helsing.Client.Audio.Api;
 using Helsing.Client.Entity.Enemy.Api;
 using UniRx;
 using Zenject;
+using Helsing.Client.Entity.Api;
+using Helsing.Client.Item;
+using Helsing.Client.Item.Api;
 
 namespace Helsing.Client.Entity.Enemy
 {
@@ -14,12 +17,22 @@ namespace Helsing.Client.Entity.Enemy
         [SerializeField]
         AudioClip playerSeen;
 
+        [SerializeField]
+        ItemData dropItem;
+
         IAudioPool audioPool;
         IMessageBroker broker;
+        IInventory inventory;
+        ILiving living;
 
         [Inject]
-        private void Inject(IAudioPool audioPool, IMessageBroker broker) =>
-            (this.audioPool, this.broker) = (audioPool, broker);
+        private void Inject(IAudioPool audioPool, IMessageBroker broker, IInventory inventory) =>
+            (this.audioPool, this.broker, this.inventory) = (audioPool, broker, inventory);
+
+        private void Awake()
+        {
+            living = GetComponent<ILiving>();
+        }
 
         private void Start()
         {
@@ -28,6 +41,12 @@ namespace Helsing.Client.Entity.Enemy
                 .Take(1)
                 .Subscribe(_ => audioPool.Next().PlayOneShot(playerSeen))
                 .AddTo(this);
+
+            living.LivesAsObservable.Where(l => l <= 0)
+                .Subscribe(_ => OnDeath())
+                .AddTo(this);
         }
+
+        private void OnDeath() => inventory.AddItem(dropItem, 1);
     }
 }
